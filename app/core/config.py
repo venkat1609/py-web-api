b8d41f895b3b133ce826eb8eb0e646cda6408c85
Venkatesh Sridhar <venkatfritz@gmail.com>
2025-09-07 16:33:23 +0400

    auth revamp

--- app/core/config.py
+++ app/core/config.py
@@ -9,9 +9,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 9999))
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 bearer_scheme = HTTPBearer()
 
--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,10 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URI = os.getenv(
-    "MONGODB_URI",
-    "mongodb+srv://admin:Venkat1995@cluster0.feoiggt.mongodb.net/?retryWrites=true&w=majority",
-)
+MONGO_URI = os.getenv("MONGODB_URI")
 
 # Initialize the MongoDB client
 client = AsyncIOMotorClient(MONGO_URI)
